2577464c8347143d1a731a44e0b84873d104ddbf
JoannaBroniarek <broniarek.joanna@gmail.com>
2020-05-04 12:43:58 +0000

    docs:finished

--- create_data.py
+++ create_data.py
@@ -14,86 +14,89 @@ import imageio
 import PIL
 import time
 
+
 def load_img(file_path, subject_id):
-  x = []
-  
-  #  Construct a file path to read an image from.
-  t1_img = os.path.join(file_path, '{}/{}_t1.nii.gz'.format(subject_id, subject_id))
+    """ 
+    Loads and preprocesses the traning image.
+    Preprocessing: 
+        - omit the initial/final slices
+        - resize image to a smaller resolution 64x64
+        - whitening
+    """
+    
+    #  Construct a file path to read an image from.
+    t1_img = os.path.join(file_path, '{}/{}_t1.nii.gz'.format(subject_id, subject_id))
 
-  # Read the .nii image containing a brain volume with SimpleITK and get 
-  # the numpy array:
-  sitk_t1 = sitk.ReadImage(t1_img)
-  t1 = sitk.GetArrayFromImage(sitk_t1)
+    # Read the .nii image containing a brain volume with SimpleITK and get 
+    # the numpy array:
+    sitk_t1 = sitk.ReadImage(t1_img)
+    t1 = sitk.GetArrayFromImage(sitk_t1)
 
-  # Select the slices from 50 to 125 among the whole 155 slices to omit initial/final slices, 
-  # since they convey a negligible amount of useful information and could affect training
-  t1 = t1[50:125]
+    # Select the slices from 50 to 125 among the whole 155 slices to omit initial/final slices, 
+    # since they convey a negligible amount of useful information and could affect training
+    t1 = t1[50:125]
   
-  # Resize images to 64 x 64 from 240 x 240
-  t1_new = np.zeros((t1.shape[0], 64, 64))
-  for i in range(t1.shape[0]):
-    t1_new[i] = cv2.resize(t1[i], dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
+    # Resize images to 64 x 64 from 240 x 240
+    t1_new = np.zeros((t1.shape[0], 64, 64))
+    for i in range(t1.shape[0]):
+        t1_new[i] = cv2.resize(t1[i], dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
 
-  # Normalise the image to zero mean/unit std dev:
-  t1 = whitening(t1_new)
+    # Normalise the image to zero mean/unit std dev:
+    t1 = whitening(t1_new)
   
-  # Create a 4D Tensor with a dummy dimension for channels
-  t1 = np.moveaxis(t1, 0, -1)
+    # Create a 4D Tensor with a dummy dimension for channels
+    t1 = np.moveaxis(t1, 0, -1)
   
-  return t1
+    return t1
 
-## copied from tensorflow site
+
+
+###########  Building the TFRecord file #############
+
+
 def _float_feature(value):
-  """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
 def _bytes_feature(value):
-  """Returns a bytes_list from a string / byte."""
-  if isinstance(value, type(tf.constant(0))):
-    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-###########
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def _decode(example_proto):
-  # Description of the features.
-  feature_description = {'t1': tf.io.FixedLenFeature([], tf.string)}
-  
-  # Parse the input `tf.Example` proto using the dictionary above.
-  features = tf.io.parse_single_example(example_proto, feature_description)
-
-  img = tf.io.parse_tensor(features['t1'], out_type=tf.float32, name=None)
-  return img
+    feature_description = {'t1': tf.io.FixedLenFeature([], tf.string)}
+    features = tf.io.parse_single_example(example_proto, feature_description)
+    img = tf.io.parse_tensor(features['t1'], out_type=tf.float32, name=None)
+    return img
 
 def parse_dataset(filename):
-  raw_dataset = tf.data.TFRecordDataset(filename)
-  return raw_dataset.map(_decode)
+    raw_dataset = tf.data.TFRecordDataset(filename)
+    return raw_dataset.map(_decode)
 
 
 def create():
-  # open the TFRecords file
-  train_filename = '../train2d.tfrecords'
-  writer = tf.io.TFRecordWriter(train_filename)
+    # open the TFRecords file
+    train_filename = '../train2d.tfrecords'
+    writer = tf.io.TFRecordWriter(train_filename)
 
-  # Iterate through directories from the training dataset
-  dataset_path = '../data/'  # os.chdir(dataset_path)
-  counter = 1
-  for subject_id in tqdm(os.listdir(dataset_path)):
-
-    # Load the image
-    img = load_img(dataset_path, subject_id)
-
-    # Create a feature
-    feature = {'t1': _bytes_feature(tf.io.serialize_tensor(img, name=None))}
+    # Iterate through directories from the training dataset
+    dataset_path = '../data/'  # os.chdir(dataset_path)
+    counter = 1
     
-    # Create an example protocol buffer
-    example = tf.train.Example(features=tf.train.Features(feature=feature))
-    
-    # Serialize to string and write on the file
-    writer.write(example.SerializeToString())
-    counter += 1
+    for subject_id in tqdm(os.listdir(dataset_path)):
 
-  writer.close()
+        # Load the image
+        img = load_img(dataset_path, subject_id)
 
+        # Create a feature
+        feature = {'t1': _bytes_feature(tf.io.serialize_tensor(img, name=None))}
 
+        # Create an example protocol buffer
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+        # Serialize to string and write on the file
+        writer.write(example.SerializeToString())
+        counter += 1
+
+    writer.close()
--- utils.py
+++ utils.py
@@ -2,24 +2,39 @@ import os
 from matplotlib import pyplot as plt
 
 def gen_and_save_images(model, epoch, test_noise, save_dir_path, gen_loss, disc_loss, show=False):
-  save_dir_path = os.path.join(save_dir_path, "images")
-  if not os.path.exists(save_dir_path):
+    """
+    Generates synthetic images while training.
+    
+    Parameters:
+    ----------
+    model - the pretrained generator model
+    epoch - the number of a current epoch
+    test_noise - the noise vector
+    save_dir_path - The output path for images
+    gen_loss, disc_loss - The geneator and discriminator losses
+    show : {boolean} - If True, it plots the images immediately 
+    
+    """
+    save_dir_path = os.path.join(save_dir_path, "images")
+    if not os.path.exists(save_dir_path):
         os.mkdir(save_dir_path)
         
-  preds = model(test_noise, training=False)
-  fig = plt.figure(figsize=(12, 10))
-  fig.suptitle("Epoch: {}\nG_Loss: {:.4f} D_Loss: {:.4f}".format(epoch, gen_loss, disc_loss, fontsize=14))
+    preds = model(test_noise, training=False)
+    fig = plt.figure(figsize=(12, 10))
+    fig.suptitle("Epoch: {}\nG_Loss: {:.4f} D_Loss: {:.4f}".format(epoch, gen_loss, disc_loss, fontsize=14))
 
-  for ind, i in enumerate(list(range(7, 70, 4))):
-    plt.subplot(4, 4, ind+1)
-    #plt.imshow(preds[0][:,:,i], cmap='gray')
-    plt.title(str(i))
-    plt.imshow(preds[0][i,:,:,0], cmap='gray')
-    plt.axis('off')
-  plt.savefig(
-      os.path.join(save_dir_path, 'img_epoch_{:04d}.png'.format(epoch)),
-      bbox_inches='tight')
-  if show:
-    plt.show()
-  plt.close()
+    for ind, i in enumerate(list(range(7, 70, 4))):
+        plt.subplot(4, 4, ind+1)
+        #plt.imshow(preds[0][:,:,i], cmap='gray')
+        plt.title(str(i))
+        plt.imshow(preds[0][i,:,:,0], cmap='gray')
+        plt.axis('off')
+  
+    plt.savefig(
+        os.path.join(save_dir_path, 'img_epoch_{:04d}.png'.format(epoch)), bbox_inches='tight')
+    
+    if show:
+        plt.show()
+    
+    plt.close()
     
